1ff82b6a35ac1095fba7245a2e1bf21574eaf1c8
Sergi <101899608+sergimcht@users.noreply.github.com>
2022-06-12 20:22:36 +0200

    Delete proyecto directory

--- proyecto/js/classTreballador.js
+++
@@ -1,9 +0,0 @@
-const arrayTreballadors = [];
-   class Treballador {
-      constructor(nom, cognom, carrec, disponibilitatTrasllat) {
-         this.nom = nom;
-         this.cognom = cognom;
-         this.carrec = carrec;
-         this.disponibilitatTrasllat = disponibilitatTrasllat;
-      }
-   }
